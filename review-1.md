ace511ae14159a8ba2b2c6212c9e7d6e7702e26e
jhonathan vergara <jhoenjv1996@gmail.com>
2023-12-14 23:48:01 -0500

    descripcion de producto

---
+++ review-1.md
@@ -0,0 +1,6 @@
+¿Qué es mi producto y para que sirve?
+esta es una aplicacion que permite al usuario realizar varias funciones en la cual la misma aplicacion te va mostrando las diferentes opciones a elejir  y sirve para crear , actualizar o borrar una o varias tareas
+
+¿Cuáles son las funcionalidades más importantes y porque los usuarios las usarían?
+
+lo mas importante de mi aplicacion es que puede crear , borrar y eliminar tareas sin complicaciones y la usarian por que intuye al cliente que debe hacer "te da diferentes opciones " sin complicarle la interaccion al usuario con la aplicacion 
--- tareas.js
+++ tareas.js
@@ -1,84 +1,111 @@
+
+const { rejects } = require("assert");
+const { resolve } = require("path");
 const readline = require("readline");
-const rl = readline.createInterface({
+const interface = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
+
+
 const listaDeTareas = [];
 
+
 function crearTarea(indicador, descripcion) {
+    return new Promise ( (resolve, rejects) => {setTimeout(() => { 
     const tarea = { indicador, descripcion, completada: false };
     listaDeTareas.push(tarea);
     console.log(`Tarea '${descripcion}' agregada correctamente.`);
+      resolve();
+}, 4000);} );
 }
 
+
 function eliminarTarea(indicador) {
-    const tareaIndex = listaDeTareas.findIndex(tarea => tarea.indicador === indicador);
+    return new Promise ((resolve , rejects) => {setTimeout(() => {
+        const tareaIndex = listaDeTareas.findIndex(tarea => tarea.indicador === indicador);
     if (tareaIndex !== -1) {
         const tarea = listaDeTareas.splice(tareaIndex, 1)[0];
         console.log(`Tarea '${tarea.descripcion}' eliminada correctamente.`);
+        resolve();
     } else {
         console.log(`No se encontró una tarea con el indicador '${indicador}'.`);
-    }
+        resolve();}
+    }, 2000);}) ;
 }
 
+
 function completarTarea(indicador) {
-    const tarea = listaDeTareas.find(tarea => tarea.indicador === indicador);
+    return new Promise((resolve, rejects)=>{setTimeout(() => {
+        const tarea = listaDeTareas.find(tarea => tarea.indicador === indicador);
     if (tarea) {
         tarea.completada = true;
         console.log(`Tarea '${tarea.descripcion}' marcada como completada.`);
+        resolve()
     } else {
         console.log(`No se encontró una tarea con el indicador '${indicador}'.`);
+        resolve();
     }
-}
+    }, 4000);});
+    
+}function mostrarMenu() {
+        console.log("Opciones:");
+        console.log("1. Agregar tarea");
+        console.log("2. Eliminar tarea");
+        console.log("3. Marcar tarea como completada");
+        console.log("4. Lista de tareas");
+        console.log("5. Salir");}
 
-function mostrarMenu() {
-    console.log("\nOpciones:");
-    console.log("1. Agregar tarea");
-    console.log("2. Eliminar tarea");
-    console.log("3. Marcar tarea como completada");
-    console.log("4. Listar tareas");
-    console.log("5. Salir");
 
-    rl.question("Selecciona una opción: ", (opcion) => {
-        switch (opcion) {
-            case "1":
-                rl.question("Introduce el indicador de la tarea: ", (indicador) => {
-                    rl.question("Introduce la descripción de la tarea: ", (descripcion) => {
-                        crearTarea(indicador, descripcion);
-                        mostrarMenu();
-                    });
-                });
-                break;
-            case "2":
-                rl.question("Introduce el indicador de la tarea a eliminar: ", (indicador) => {
-                    eliminarTarea(indicador);
-                    mostrarMenu();
-                });
-                break;
-            case "3":
-                rl.question("Introduce el indicador de la tarea a marcar como completada: ", (indicador) => {
-                    completarTarea(indicador);
-                    mostrarMenu();
-                });
-                break;
-            case "4":
-                console.log("Lista de Tareas:");
-                listaDeTareas.forEach((tarea) => {
-                    const estado = tarea.completada ? "Completada" : "No Completada";
-                    console.log(`Indicador: ${tarea.indicador}, Descripción: ${tarea.descripcion}, Estado: ${estado}`);
-                });
-                mostrarMenu();
-                break;
-            case "5":
-                console.log("¡Hasta luego!");
-                rl.close();
-                break;
-            default:
-                console.log("Opción no válida. Por favor, selecciona una opción válida.");
-                mostrarMenu();
-        }
+
+
+async function ejecutarMenu() {
+    while (true) {
+      mostrarMenu();
+      const opcion = await pregunta("Selecciona una opción: ");
+      switch (opcion) {
+        case "1":
+          const indicadorAgregar = await pregunta("Introduce el indicador de la tarea: ");
+          const descripcionAgregar = await pregunta("Introduce la descripción de la tarea: ");
+          await crearTarea(indicadorAgregar, descripcionAgregar);
+          break;
+        case "2":
+          const indicadorEliminar = await pregunta("Introduce el indicador de la tarea a eliminar: ");
+          await eliminarTarea(indicadorEliminar);
+          break;
+        case "3":
+          const indicadorCompletar = await pregunta("Introduce el indicador de la tarea a marcar como completada: ");
+          await completarTarea(indicadorCompletar);
+          break;
+        case "4":
+          console.log("Lista de Tareas:");
+          listaDeTareas.forEach((tarea) => {
+            const estado = tarea.completada ? "Completada" : "No Completada";
+            console.log(`Indicador: ${tarea.indicador}, Descripción: ${tarea.descripcion}, Estado: ${estado}`);
+          });
+          break;
+        case "5":
+          console.log("¡Hasta luego!");
+          interface.close();
+          return;
+        default:
+          console.log("Opción no válida. Por favor, selecciona una opción válida.");
+      }
+    }
+  }
+  
+  async function pregunta(pregunta) {
+    return new Promise((resolve) => {
+      interface.question(pregunta, (respuesta) => {
+        resolve(respuesta);
+      });
     });
-}
-
-mostrarMenu();
+  }
+  
+  async function iniciarPrograma() {
+    await ejecutarMenu();
+  }
+  
+  iniciarPrograma();
+  
